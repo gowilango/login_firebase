87c729af2c3274291629c1a9865eecc947ef0873
Gowtham ILangovan <gowtham15164@gmail.com>
2018-03-13 23:40:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAhBaj1kFA1vjx97Y55ozvPmJiZNjYEo7Q",
+      authDomain: "cs-16-20.firebaseapp.com",
+      databaseURL: "https://cs-16-20.firebaseio.com",
+      projectId: "cs-16-20",
+      storageBucket: "cs-16-20.appspot.com",
+      messagingSenderId: "543772938464"
+    };
+    firebase.initializeApp(config);
+  
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+   
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+      
+     
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
